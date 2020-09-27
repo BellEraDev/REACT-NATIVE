b70222b305cfb873b30be8220c864297f477c38c
Watcharapun Wattanasombut <67476386+BellEraDev@users.noreply.github.com>
2020-09-27 10:14:44 +0700

    Test_RNN
    React-Native Build Mobile-App

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ // * @format
+ */
+
+// import {AppRegistry} from 'react-native';
+// import {name as appName} from './app.json';
+// import App from './App';
+// import PickerDemo from './src/PickerDemo';
+
+// AppRegistry.registerComponent(appName, () => PickerDemo);
+import ScreenManager from './src/screens/ScreenManager';
